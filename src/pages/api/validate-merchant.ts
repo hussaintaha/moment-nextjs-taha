d835b59c2d790202290ddf8dfaf3dcb642ac3781
husaintaha <hussaintaha500@gmail.com>
2024-10-26 11:35:20 +0530

    added a cert and key file

--- src/pages/api/validate-merchant.ts
+++ src/pages/api/validate-merchant.ts
@@ -1,7 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import https from 'https';
 import path from 'path';
-import { promises as fs } from 'fs';
+// import { promises as fs } from 'fs';
+import * as fs from 'fs';
 
 interface ApiResponse {
     status: number;
@@ -12,25 +13,35 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         try {
             // const certPath = process.env.APPLE_PAY_CERT_PATH;
+            const certPath = path.resolve('./public', 'file-c', 'ApplePay.crt.pem')
+            const keyPath = path.resolve('./public', 'file-c', 'ApplePay.key.pem')
 
-
-            const certPath = path.resolve('./public', 'file-c', 'apple_pay.pem')
             console.log("certPath", certPath);
+            console.log("keyPath", keyPath);
 
-            if (!certPath) {
-                return res.status(500).json({ error: 'Certificate path is not defined.' });
+
+            if (!certPath || !keyPath) {
+                return res.status(500).json({ error: 'Certificate path or Key path is not defined.' });
             }
-            const cert = await fs.readFile(certPath, 'utf8');
+
+            const cert = await fs.readFileSync(certPath, 'utf8');
+            const key = await fs.readFileSync(keyPath, 'utf8');
+
             console.log("cert", cert);
+            console.log("key", key);
+
 
             const agent = new https.Agent({
-                cert: certPath,
+                cert: cert,
+                key: key,
+                // passphrase: "letsgetlost100!"
             });
+
+            console.log("agent", agent.options.cert);
 
             const { validationURL } = req.body;
 
             console.log("validationURL", validationURL);
-
 
             if (!validationURL) {
                 return res.status(400).json({ error: 'Validation URL is required.' });
@@ -43,15 +54,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 initiative: 'web',
                 initiativeContext: 'moment-nextjs-taha.vercel.app',
             });
-// BEGIN CERTIFICATE
-// END CERTIFICATE
+
             const response = await new Promise((resolve, reject) => {
                 const request = https.request(validationURL, {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    agent
+                    cert: cert,
+                    key: key,
+                    passphrase: "letsgetlost100!"
                 }, (response) => {
                     let data = '';
                     response.on('data', (chunk) => {
@@ -84,55 +96,3 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
-
-
-
-
-
-
-// // pages/api/merchant/validate.js
-
-// import { NextApiRequest, NextApiResponse } from 'next';
-// import https from 'https';
-// import axios from 'axios';
-// import { promises as fs } from 'fs';
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//     if (req.method === 'POST') {
-//         const { appleUrl } = req.body;
-
-//         try {
-//             const [certificate, key] = await Promise.all([
-//                 fs.readFile(config.keyFilePath.cert), // Adjust path for the certificate
-//                 fs.readFile(config.keyFilePath.key),   // Adjust path for the key
-//             ]);
-
-//             const httpsAgent = new https.Agent({
-//                 rejectUnauthorized: false,
-//                 cert: certificate,
-//                 key: key,
-//                 passphrase: config.passphrase,
-//             });
-
-//             const response = await axios.post(
-//                 appleUrl,
-//                 {
-//                     merchantIdentifier: config.merchantIdentifier,
-//                     domainName: config.yourDomain,
-//                     displayName: config.merchantName,
-//                 },
-//                 { httpsAgent }
-//             );
-
-
-//             console.info(`Successfully validated the merchant: ${response.data}`);
-//             res.status(200).json(response.data);
-//         } catch (error: any) {
-//             console.error(`Error validating merchant: ${error.message}`);
-//             res.status(500).send('Error getting validation from Apple Pay');
-//         }
-//     } else {
-//         res.setHeader('Allow', ['POST']);
-//         res.status(405).end(`Method ${req.method} Not Allowed`);
-//     }
-// }
